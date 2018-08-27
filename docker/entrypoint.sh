8aa8ae82fd9445a621bc59a995e81ded365dc9bc
Andrew Rabert <ar@nullsum.net>
2018-08-27 14:53:57 -0400

    Add sudo

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 set -e
 adduser -u $PUID -s /bin/sh -h $PHOME -H $PUSER -D
+cat >> /etc/sudoers<<EOF
+$PUSER ALL=(ALL) NOPASSWD: ALL
+EOF
 export -n PGID
 export -n PHOME
 export -n PUID
